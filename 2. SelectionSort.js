49d42cf76017b539ad11df19212f4eb69aa3b280
Ashfaque Hossain Abir <108783774+abirett5@users.noreply.github.com>
2023-09-15 19:49:03 +0600

    Files Added

---
+++ 2. SelectionSort.js
@@ -0,0 +1,5 @@
+// Ques 2: Implement Selection Sort in JavaScript
+// Write a function to sort the given array nums in ascending order.
+
+// Input: nums = [29,10,14,37,14]  ----->>>>>  Output: [10,14,14,29,37]
+
---
+++ 3. InsertionSort.js
@@ -0,0 +1,5 @@
+// Ques 3: Implement Insertion Sort in JavaScript
+// Write a function to sort the given array nums in ascending order.
+
+// Input: nums = [29,10,14,37,14,33,8,11] ----->>>>>  Output: [8,10,11,14,14,29,33,37]
+
